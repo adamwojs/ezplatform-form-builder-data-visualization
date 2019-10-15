90717e4bd02e5a8f7440f69e4e0d2edb5b2488d5
Adam Wójs <adam@wojs.pl>
2019-10-15 20:46:23 +0200

    Impl. filtering data by language

--- src/bundle/UI/Tab/ReportTab.php
+++ src/bundle/UI/Tab/ReportTab.php
@@ -6,6 +6,9 @@ namespace AdamWojs\EzPlatformFormBuilderReportBundle\UI\Tab;
 
 use AdamWojs\EzPlatformFormBuilderReport\API\FormSubmissionReportServiceInterface;
 use eZ\Publish\API\Repository\ContentTypeService;
+use eZ\Publish\API\Repository\LanguageService;
+use eZ\Publish\API\Repository\Values\Content\Content;
+use eZ\Publish\API\Repository\Values\Content\Language;
 use EzSystems\EzPlatformAdminUi\Tab\AbstractTab;
 use EzSystems\EzPlatformAdminUi\Tab\ConditionalTabInterface;
 use EzSystems\EzPlatformAdminUi\Tab\OrderedTabInterface;
@@ -15,7 +18,9 @@ use Twig\Environment;
 
 final class ReportTab extends AbstractTab implements OrderedTabInterface, ConditionalTabInterface
 {
-    private const TAB_IDENTIFIER = 'submissions_report';
+    public const URI_FRAGMENT = 'ez-tab-location-view-submission-summary';
+
+    private const TAB_IDENTIFIER = 'submission-summary';
 
     /** @var FormSubmissionReportServiceInterface */
     private $formSubmissionReportService;
@@ -23,21 +28,31 @@ final class ReportTab extends AbstractTab implements OrderedTabInterface, Condit
     /** @var \eZ\Publish\API\Repository\ContentTypeService */
     private $contentTypeService;
 
+    /** @var \eZ\Publish\API\Repository\LanguageService */
+    private $languageService;
+
     /** @var \EzSystems\EzPlatformFormBuilder\FieldType\Type */
     private $formBuilderType;
+
+    /** @var array */
+    private $siteAccessLanguages;
 
     public function __construct(
         Environment $twig,
         TranslatorInterface $translator,
         FormSubmissionReportServiceInterface $formSubmissionReportService,
         ContentTypeService $contentTypeService,
-        Type $formBuilderType
+        LanguageService $languageService,
+        Type $formBuilderType,
+        array $siteAccessLanguages
     ) {
         parent::__construct($twig, $translator);
 
-        $this->contentTypeService = $contentTypeService;
-        $this->formBuilderType = $formBuilderType;
         $this->formSubmissionReportService = $formSubmissionReportService;
+        $this->contentTypeService = $contentTypeService;
+        $this->languageService = $languageService;
+        $this->formBuilderType = $formBuilderType;
+        $this->siteAccessLanguages = $siteAccessLanguages;
     }
 
     public function evaluate(array $parameters): bool
@@ -78,13 +93,50 @@ final class ReportTab extends AbstractTab implements OrderedTabInterface, Condit
         /** @var \eZ\Publish\API\Repository\Values\Content\Content $content */
         $content = $parameters['content'];
 
-        $report = $this->formSubmissionReportService->generate($content->contentInfo);
+        $report = $this->formSubmissionReportService->generate(
+            $content->contentInfo,
+            $content->prioritizedFieldLanguageCode
+        );
 
         return $this->twig->render(
             '@ezdesign/content/tab/report/tab.html.twig',
             array_merge($parameters, [
                 'report' => $report,
+                'languages' => $this->loadContentLanguages($content),
             ])
         );
+    }
+
+    /**
+     * Loads system languages with filtering applied.
+     *
+     * @param \eZ\Publish\API\Repository\Values\Content\Content $content
+     *
+     * @return array
+     */
+    private function loadContentLanguages(Content $content): array
+    {
+        $contentLanguages = $content->versionInfo->languageCodes;
+
+        $filter = function (Language $language) use ($contentLanguages) {
+            return $language->enabled && in_array($language->languageCode, $contentLanguages, true);
+        };
+
+        $languagesByCode = [];
+        foreach (array_filter($this->languageService->loadLanguages(), $filter) as $language) {
+            $languagesByCode[$language->languageCode] = $language;
+        }
+
+        $saLanguages = [];
+        foreach ($this->siteAccessLanguages as $languageCode) {
+            if (!isset($languagesByCode[$languageCode])) {
+                continue;
+            }
+
+            $saLanguages[] = $languagesByCode[$languageCode];
+            unset($languagesByCode[$languageCode]);
+        }
+
+        return array_merge($saLanguages, array_values($languagesByCode));
     }
 }
